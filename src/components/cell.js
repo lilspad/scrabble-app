091d14b10285be1baeb97a1f6761824a76b86dd0
lilspad <lilypaczesniak@outlook.com>
2022-09-18 00:27:35 +0100

    functional component

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,45 +1,9 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 
-const cellTypes={
-    0: {
-        display: "-",
-        color: "plain"
-    },
-    1: {
-        display: "DLS",
-        color: "sage"
-    },
-    2: {
-        display: "TLS",
-        color: "lilac"
-    },
-    3: {
-        display: "DWS",
-        color: "tange"
-    },
-    4: {
-        display: "TWS",
-        color: "rust"
-    },
-    5: {
-        display: "*",
-        color: "rays"
-    }
-};
-class Cell extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            display: "-",
-            color: "plain"
-        };
-    }
-
-    render() {
-        return (
-            <div>{}</div>
-        );
-    }
+export default function Cell(props) {
+    const [display, setDisplay] = useState(0);
+    useEffect(() => {setDisplay(props.display)}, [props.display])
+    return (
+        <div>{display}</div>
+    )
 }
-
-export default Cell;
