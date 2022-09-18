3fd218178b477e72b92b1a189edc478c8ee363ec
lilspad <lilypaczesniak@outlook.com>
2022-09-18 19:59:13 +0100

    clean up unused vars and change display values

--- src/components/board.js
+++ src/components/board.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import Cell from './cell.js';
 
-/* const boardCells = [
-    [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4],
-    [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0],
-    [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0],
-    [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1],
-    [0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
-    [0,2,0,0,0,2,0,0,0,2,0,0,0,2,0],
-    [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
-    [4,0,0,1,0,0,0,5,0,0,0,1,0,0,4],
-    [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
-    [0,2,0,0,0,2,0,0,0,2,0,0,0,2,0],
-    [0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
-    [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1],
-    [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0],
-    [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0],
-    [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4]
-]; */
+const cellTypes={
+    0: {
+        "display": "---",
+        color: "plain"
+    },
+    1: {
+        "display": "oo",
+        color: "sage"
+    },
+    2: {
+        "display": "fff",
+        color: "lilac"
+    },
+    3: {
+        "display": "??",
+        color: "tange"
+    },
+    4: {
+        "display": "!!!",
+        color: "rust"
+    },
+    5: {
+        "display": "**",
+        color: "rays"
+    }
+};
 
-const row1 = [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4];
+const row1 = [4,0,0,0,1,0,0,4,0,0,1,0,0,0,4];
 const row2 = [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0];
 const row3 = [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0];
 const row4 = [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1];
@@ -30,58 +39,33 @@ const row8 = [4,0,0,1,0,0,0,5,0,0,0,1,0,0,4];
 
 const rowPatterns = [row1, row2, row3, row4, row5, row6, row7, row8]
 
-const cellTypes={
-    0: {
-        "display": "xxx",
-        color: "plain"
-    },
-    1: {
-        "display": "DLS",
-        color: "sage"
-    },
-    2: {
-        "display": "TLS",
-        color: "lilac"
-    },
-    3: {
-        "display": "DWS",
-        color: "tange"
-    },
-    4: {
-        "display": "TWS",
-        color: "rust"
-    },
-    5: {
-        "display": "*",
-        color: "rays"
-    }
-};
-
 class Board extends React.Component {
     createRow(row) {
         
         let boardRow = row.map((num, index) => {
             let display = cellTypes[num].display;
             return (
-                <Cell key={index} id="cell${index}" 
-                className="cell" display={display} type="num"/>
+                <Cell key={index} className="cell" 
+                display={display} type="num"/>
             )
         })
         return boardRow;
     }
 
-    createBoard() {
-
+    createBoard(pattern) {
+        let rowPattern = pattern.map((arr, index) => {
+            return (
+            <tr>{this.createRow(arr)}</tr>
+        )
+        })
+        return rowPattern;
     }
 
     render() {
-        const status = "Rendering ok from Board";
         return (
             <div>
             <table>
-            <tr>{this.createRow(row1)}</tr>
-            <tr>{this.createRow(row2)}</tr>
-            <tr>{this.createRow(row3)}</tr>
+            {this.createBoard(rowPatterns)}
             </table>         
             </div>
         )
