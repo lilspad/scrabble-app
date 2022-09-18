7e3966f0c774398da0afe3388e9821d6e2e49b60
lilspad <lilypaczesniak@outlook.com>
2022-09-18 23:05:44 +0100

    Update game.js

--- src/components/game.js
+++ src/components/game.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Board from './board.js';
+import Board from './board/board.js';
 
 
 
 class Game extends React.Component {
     render() {
-        const status = "Rendering ok from Game"
+        const status = "Currrent score: 0"
         return (
         <div>
-        <div id="status">{status}</div>
+        <h3 id="status">{status}</h3>
         <div className="board"><Board /></div>
         </div>)
     }
