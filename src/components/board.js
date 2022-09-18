0f8513ae1433c5d907daaf54346fb6420117c6da
lilspad <lilypaczesniak@outlook.com>
2022-09-18 16:30:16 +0100

    render rows using <table> in board and cell as a <td>

--- src/components/board.js
+++ src/components/board.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Cell from './cell.js';
-import cellTypes from './cellTypes';
 
-const boardCells = [
+/* const boardCells = [
     [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4],
     [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0],
-    [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1],
     [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0],
+    [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1],
     [0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
     [0,2,0,0,0,2,0,0,0,2,0,0,0,2,0],
     [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
@@ -18,25 +17,72 @@ const boardCells = [
     [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0],
     [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0],
     [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4]
-];
+]; */
+
+const row1 = [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4];
+const row2 = [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0];
+const row3 = [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0];
+const row4 = [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1];
+const row5 = [0,0,0,0,3,0,0,0,0,0,3,0,0,0,0];
+const row6 = [0,2,0,0,0,2,0,0,0,2,0,0,0,2,0];
+const row7 = [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0];
+const row8 = [4,0,0,1,0,0,0,5,0,0,0,1,0,0,4];
+
+const rowPatterns = [row1, row2, row3, row4, row5, row6, row7, row8]
+
+const cellTypes={
+    0: {
+        "display": "xxx",
+        color: "plain"
+    },
+    1: {
+        "display": "DLS",
+        color: "sage"
+    },
+    2: {
+        "display": "TLS",
+        color: "lilac"
+    },
+    3: {
+        "display": "DWS",
+        color: "tange"
+    },
+    4: {
+        "display": "TWS",
+        color: "rust"
+    },
+    5: {
+        "display": "*",
+        color: "rays"
+    }
+};
 
 class Board extends React.Component {
+    createRow(row) {
+        
+        let boardRow = row.map((num, index) => {
+            let display = cellTypes[num].display;
+            return (
+                <Cell key={index} id="cell${index}" 
+                className="cell" display={display} type="num"/>
+            )
+        })
+        return boardRow;
+    }
 
     createBoard() {
-        const board = boardCells.map(row => {
-            row.map(col => {
-                Cell(cellTypes[col])
-            })
-        })
-        return board;
+
     }
 
     render() {
         const status = "Rendering ok from Board";
         return (
             <div>
-            <div id="status">{status}</div>
-            {this.createBoard()}
+            <table>
+            <tr>{this.createRow(row1)}</tr>
+            <tr>{this.createRow(row2)}</tr>
+            <tr>{this.createRow(row3)}</tr>
+            </table>         
             </div>
         )
     }
--- src/components/cell.js
+++ src/components/cell.js
@@ -1,9 +1,6 @@
-import {useState, useEffect} from 'react';
 
 export default function Cell(props) {
-    const [display, setDisplay] = useState(0);
-    useEffect(() => {setDisplay(props.display)}, [props.display])
-    return (
-        <div>{display}</div>
-    )
-}
+        return (
+            <td>[{props.display}]</td>
+        )
+    }
