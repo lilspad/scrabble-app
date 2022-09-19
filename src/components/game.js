38c9acffc76290523a4df37591d6c5b73ba77593
lilspad <lilypaczesniak@outlook.com>
2022-09-20 00:16:04 +0100

    display bag, rack and add player status

--- src/components/game.js
+++ src/components/game.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Board from './board/board.js';
 import Bag from './tiles/bag.js';
+import Rack from './tiles/rack.js'
 
-
-
+const flex = {
+    display: "flex",
+    columnGap: "10px"
+}
 class Game extends React.Component {
     render() {
-        const status = "Currrent score: 0"
+        const scoreStatus = "Currrent score: 0";
+        const playerStatus = "Current turn: Player x"
         return (
         <div className="game">
-        <h3 className="status">{status}</h3>
-        <div classNAme="bag"><Bag /></div>
+        <div style={flex}>
+        <h3 className="status">{scoreStatus}</h3>
+        <h3 className="status">{playerStatus}</h3>
+        </div>
+        <div style={flex}>
         <div className="board"><Board /></div>
+        <div classNAme="bag"><Bag /></div>
+        </div>
+        <div><Rack /></div>
         </div>)
     }
 }
