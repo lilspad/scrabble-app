d571aaddc6444c254258b9c9d08787762d17fe60
lilspad <lilypaczesniak@outlook.com>
2023-01-22 14:08:44 +0000

    update game display

--- src/components/game.js
+++ src/components/game.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import Board from './board/board.js';
 import Bag from './tiles/bag.js';
-import Rack from './tiles/rack.js'
+import Rack from './tiles/rack.js';
 
 class Game extends React.Component {
     render() {
-        const scoreStatus = "Currrent score: 0";
-        const playerStatus = "Current turn: Player x"
+
+        const scoreX = 0;
+        const scoreY = 0;
+
         return (
-        <div className="game">
-        <div className="div">
-        <div className="board"><Board /></div>
-        <div style={{textAlign: "center"}}>
-            <div className="bag"><Bag /></div>
-            <h3>{scoreStatus}</h3> <br></br>
-            <h3>{playerStatus}</h3>
-        </div>
-        </div>
-        <div><Rack /></div>
-        </div>)
+            <div className="game">
+                <div className="div">
+                    <div className="board"><Board /></div>
+                    <div style={{textAlign: "center"}}>
+                    
+                        <h3>TURN STATUS</h3>
+                        <h2>Score:</h2>
+                        <div className="div score">
+                            <h3>Player X: {scoreX}</h3>
+                            <h3>Player Y: {scoreY}</h3>
+                        </div>
+                        
+                        <div className="bag"><Bag /></div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
