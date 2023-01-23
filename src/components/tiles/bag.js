af010fbbc8221739877cb26f4c0051056068805f
lilspad <lilypaczesniak@outlook.com>
2023-01-23 02:07:11 +0000

    move rack comp to game

--- src/components/tiles/bag.js
+++ src/components/tiles/bag.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import Rack from './rack.js'
 import Tile from './tile.js'
 import letters from './letters.js';
 
 const bagStyle = {
     display: "flex",
     flexWrap: "wrap",
-    justifyContent: "space-evenly",
-    alignContent: "center"
+    justifyContent: "space-evenly"
 }
 
 const divStyle = {
-    backgroundColor: "#5c7551",
     textAlign: "center",
     width: "60%",
     margin: "auto"
 }
 
+
+
 class Bag extends React.Component {
 
     createTiles() {
         return (
-            <div className="bag" style={bagStyle}>
+            <div style={bagStyle}>
                 {Object.keys(letters).map((key) => {
                     return (
                         <div className="tileCell bagCell"  key={key}>
@@ -39,7 +38,6 @@ class Bag extends React.Component {
             <h3>Tiles in the bag:</h3>
                 {this.createTiles()}
                 <div>Total: </div>
-                <Rack />
             </div>
         )
     }
