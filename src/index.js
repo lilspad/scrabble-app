df9f683726bdf755ef0b7895569cc0d0f4c54593
lilspad <lilypaczesniak@outlook.com>
2023-01-23 02:08:45 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Game from './components/game.js';
 import "./style.css"
 
-document.body.style = 'background: #5c7551';
+document.body.style = 'background: #5c7551; margin: 0';
 
 class App extends React.Component {
     render() {
