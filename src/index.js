7cce0137194cbfeca7ba877c923db7667ebdb5d3
lilspad <lilypaczesniak@outlook.com>
2023-02-14 12:35:32 +0000

    add to do notes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ document.body.style = 'background: #5c7551; margin: 0';
 
 class App extends React.Component {
     render() {
-        const title = "Scrabble";
+        const title = "Wildwood Scrabble";
         return (
             <div>
             <h1>{title}</h1>
@@ -19,3 +19,14 @@ class App extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+/* To do:
+
+- Fix bug where you click on a cell WITH a tile already on it, AND have a selected tile in hand, the selected tile disappears (needs to be sent back to rack)
+- Fix bug where after removing a tile off a cell, that cell no longer accepts tiles
+- Define rules for placement: 
+    - First word has to touch center
+    - All words thereafter have to be attached to another word
+- Execute checks on PLAY
+
+*/
