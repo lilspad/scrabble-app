d29c53d29738e435c23b11e59e9906ce82c8a5c5
lilspad <lilypaczesniak@outlook.com>
2023-02-14 13:05:57 +0000

    move to do to notes.txt

--- src/index.js
+++ src/index.js
@@ -19,14 +19,3 @@ class App extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-
-/* To do:
-
-- Fix bug where you click on a cell WITH a tile already on it, AND have a selected tile in hand, the selected tile disappears (needs to be sent back to rack)
-- Fix bug where after removing a tile off a cell, that cell no longer accepts tiles
-- Define rules for placement: 
-    - First word has to touch center
-    - All words thereafter have to be attached to another word
-- Execute checks on PLAY
-
-*/
